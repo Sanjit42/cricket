123a9315fa324bfd7d44ae5d11ae4e2845538270
Sanjit42 <sanjitprad@gmail.com>
2017-12-06 12:07:16 +0530

    Create tieBreakerMatch directory

---
+++ tieBreakerMatch/src/main/java/com/cricket/tieBreakerMatch/TieBreakerMatchApplication.java
@@ -0,0 +1,12 @@
+package com.cricket.tieBreakerMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TieBreakerMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TieBreakerMatchApplication.class, args);
+	}
+}
---
+++ tieBreakerMatch/src/test/java/com/cricket/tieBreakerMatch/TieBreakerMatchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cricket.tieBreakerMatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TieBreakerMatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
